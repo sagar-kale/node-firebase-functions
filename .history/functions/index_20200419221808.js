3813125b8c582782f70ee4664c6af06be0d1afe4
sagar-kale <sagark.kale@hotmail.com>
2020-04-19 22:37:01 +0530

    react redux firebase node trigers

---
+++ .history/functions/index_20200419221808.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.region('asia-east2').https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+// change  photo trigger
+
+exports.onUserImageChange = functions
+    .region('asia-east2')
+    .firestore.document('/users/{userId}')
+    .onUpdate((change) => {
+        console.log(change.before.data());
+        console.log(change.after.data());
+        if (change.before.data().photoURL !== change.after.data().photoURL) {
+            console.log('image has changed');
+            const batch = db.batch();
+            return db
+                .collection('projects')
+                .where('userHandle', '==', change.before.data().handle)
+                .get()
+                .then((data) => {
+                    data.forEach((doc) => {
+                        const scream = db.doc(`/projects/${doc.id}`);
+                        batch.update(scream, { userImage: change.after.data().photoURL });
+                    });
+                    return batch.commit();
+                });
+        }
+        return true;
+    });
